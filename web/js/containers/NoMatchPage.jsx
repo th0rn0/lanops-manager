a052374d3a5531b497a5dc661d16f2fb5ee2b889
Unknown <dev@th0rn0.co.uk>
2018-01-22 19:12:27 +0000

    Merge branch 'develop'

--- web/js/containers/NoMatchPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class NoMatchPage extends React.Component {
-  render() {
-    return <div className="container">Page not found!</div>
-  }
-}
-
-export default NoMatchPage;
