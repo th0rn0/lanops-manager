4e0340b94ceb7eea4bf548f450df92645cdcdff3
Unknown <dev@th0rn0.co.uk>
2018-02-04 16:03:37 +0000

    Route file clean up

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,63 +1,78 @@
 <?php
 
-//Login
+/**
+ * Login & Register
+ */
 Route::get('social/login/redirect/{provider}', ['uses' => 'Auth\AuthController@redirectToProvider', 'as' => 'social.login']);
 Route::get('social/login/{provider}', 'Auth\AuthController@handleProviderCallback');
 Route::post('/steamlogin/register/{user}', 'AuthController@store');
+Route::group(['middleware' => ['web']], function () {
+	
+	Route::get('/steamlogin', 'AuthController@login');
+	
+	Route::group(['middleware' => ['auth']], function () {
+		Route::get('/account', 'AccountController@index');
+		Route::get('logout', 'AuthController@doLogout');
+
+		Route::get('/steamlogin/register/{user}', 'AuthController@register');
+		Route::patch('/steamlogin/register/{user}', 'AuthController@update');
+	});
+});
 
 
 Route::group(['middleware' => ['web']], function () {
 
-	// API ROUTES
-	// Timetables
+	/**
+	 * API
+	 */
 	Route::get('/api/events/{event}/timetables', 'Events\TimetablesController@index');
 	Route::get('/api/events/{event}/timetables/{timetable}', 'Events\TimetablesController@show');
 
-	// Participants
 	Route::get('/api/events/{event}/participants', 'Events\ParticipantsController@show');
 
-	// Tickets
 	Route::get('/api/events/{event}/tickets', 'Events\TicketsController@index');
 	Route::get('/api/events/{event}/tickets/{ticket}', 'Events\TicketsController@show');
 
-	// Login
-	Route::get('/steamlogin', 'AuthController@login');
-	Route::group(['middleware' => ['auth']], function () {
-		//User Control
-		Route::get('/account', 'AccountController@index');
-		//Logout
-		Route::get('logout', 'AuthController@doLogout');
-		//Register
-		Route::get('/steamlogin/register/{user}', 'AuthController@register');
-		Route::patch('/steamlogin/register/{user}', 'AuthController@update');
-	});
-
-	// Index
+	/**
+	 * Index Page
+	 */
 	Route::get('/', 'HomeController@index');
 
-	// Event
+	/**
+	 * Events
+	 */
 	Route::get('/events', 'Events\EventsController@index');
 	Route::get('/events/{event}', 'Events\EventsController@show');
+	Route::get('/events/{event}/big', 'HomeController@bigScreen');
 
-	// Misc
+	/**
+	 * Misc pages
+	 */
 	Route::get('/about', 'HomeController@about');
 	Route::get('/contact', 'HomeController@contact');
 
-	// Tickets & Gifts
+	/**
+	 * Tickets
+	 */
 	Route::get('/tickets/retrieve/{participant}', 'Events\TicketsController@retrieve');
-	Route::post('/events/{event}/tickets/{ticket}/purchase/{user}/gift', 'Events\TicketsController@purchaseGift');
+	Route::post('/tickets/purchase/{ticket}', 'Events\TicketsController@purchase');
+
+	/**
+	 * Gifts
+	 */
 	Route::get('/gift/accept', 'Events\ParticipantsController@acceptGift');
 	Route::post('/gift/{participant}', 'Events\ParticipantsController@gift');
 	Route::post('/gift/{participant}/revoke', 'Events\ParticipantsController@revokeGift');
 
-	// Gallery Routes
+	/**
+	 * Galleries
+	 */
 	Route::get('/gallery', 'GalleryController@index');
 	Route::get('/gallery/{album}', 'GalleryController@show');
 
-	// Annoucement & Intranet Page
-	Route::get('/events/{event}/big', 'HomeController@bigScreen');
-
-	// Tournaments
+	/**
+	 * Tournaments
+	 */
 	Route::get('/events/{event}/tournaments', 'Events\TournamentsController@index');
 	Route::get('/events/{event}/tournaments/{tournament}', 'Events\TournamentsController@show');
 	Route::post('/events/{event}/tournaments/{tournament}/register', 'Events\TournamentsController@register');
@@ -65,10 +80,9 @@ Route::group(['middleware' => ['web']], function () {
 	Route::post('/events/{event}/tournaments/{tournament}/register/pug', 'Events\TournamentsController@registerPug');
 	Route::post('/events/{event}/tournaments/{tournament}/register/remove', 'Events\TournamentsController@unregister');
 
-	// Purchase
-	Route::post('/tickets/purchase/{ticket}', 'Events\TicketsController@purchase');
-
-	// Payments
+	/**
+	 * Payments
+	 */
 	Route::get('/payment/review', 'PaymentsController@review');
 	Route::get('/payment/callback', 'PaymentsController@process');
 	Route::post('/payment/post', 'PaymentsController@post');
@@ -76,24 +90,36 @@ Route::group(['middleware' => ['web']], function () {
 	Route::get('/payment/cancelled', 'PaymentsController@cancelled');
 	Route::get('/payment/successful/{purchase}', 'PaymentsController@successful');
 
-	// Seating
+	/**
+	 * Seating
+	 */
 	Route::post('/events/{event}/seating/{seating_plan}', 'Events\SeatingController@store');
 	Route::delete('/events/{event}/seating/{seating_plan}', 'Events\SeatingController@destroy');
 
-	// Admin User Routes group
+	/**
+	 * Admin
+	 */
 	Route::group(['middleware' => ['admin']], function () {
 
-		// Index
+		/**
+		 * Index Page
+		 */
 		Route::get('/admin', 'Admin\AdminController@index');
 
-		// Events
+		/**
+		 * Events
+		 */
 		Route::get('/admin/events', 'Admin\Events\EventsController@index');
 		Route::post('/admin/events', 'Admin\Events\EventsController@store');
 		Route::get('/admin/events/{event}', 'Admin\Events\EventsController@show');
 		Route::post('/admin/events/{event}', 'Admin\Events\EventsController@update');
 		Route::delete('/admin/events/{event}', 'Admin\Events\EventsController@destroy');
-
-		// Seating
+		Route::post('/admin/events/{event}/information', 'Admin\Events\InformationController@store');
+		Route::post('/admin/information/{information}', 'Admin\Events\InformationController@update');
+		Route::delete('/admin/information/{information}', 'Admin\Events\InformationController@destroy');
+		/**
+		 * Seating
+		 */
 		Route::get('/admin/events/{event}/seating', 'Admin\Events\SeatingController@index');
 		Route::post('/admin/events/{event}/seating', 'Admin\Events\SeatingController@store');
 		Route::get('/admin/events/{event}/seating/{seating_plan}', 'Admin\Events\SeatingController@show');
@@ -102,18 +128,20 @@ Route::group(['middleware' => ['web']], function () {
 		Route::post('/admin/events/{event}/seating/{seating_plan}/seat', 'Admin\Events\SeatingController@storeSeat');
 		Route::delete('/admin/events/{event}/seating/{seating_plan}/seat', 'Admin\Events\SeatingController@destroySeat');
 
-		// Timetable
+		/**
+		 * Timetables
+		 */
 		Route::get('/admin/events/{event}/timetables', 'Admin\Events\TimetablesController@index');
 		Route::post('/admin/events/{event}/timetables', 'Admin\Events\TimetablesController@store');
 		Route::get('/admin/events/{event}/timetables/{timetable}', 'Admin\Events\TimetablesController@show');
 		Route::post('/admin/events/{event}/timetables/{timetable}', 'Admin\Events\TimetablesController@update');
 		Route::delete('/admin/events/{event}/timetables/{timetable}', 'Admin\Events\TimetablesController@destroy');
-
-		// Timetable Data
 		Route::post('/admin/events/{event}/timetables/{timetable}/data', 'Admin\Events\TimetableDataController@store');
 		Route::post('/admin/events/{event}/timetables/{timetable}/data/{data}', 'Admin\Events\TimetableDataController@update');
 
-		// Tournaments
+		/**
+		 * Tournaments
+		 */
 		Route::get('/admin/events/{event}/tournaments', 'Admin\Events\TournamentsController@index');
 		Route::post('/admin/events/{event}/tournaments', 'Admin\Events\TournamentsController@store');
 		Route::get('/admin/events/{event}/tournaments/{tournament}', 'Admin\Events\TournamentsController@show');
@@ -123,35 +151,44 @@ Route::group(['middleware' => ['web']], function () {
 		Route::post('/admin/events/{event}/tournaments/{tournament}/finalize', 'Admin\Events\TournamentsController@finalize');
 		Route::post('/admin/events/{event}/tournaments/{tournament}/participants/{participant}/team', 'Admin\Events\TournamentsController@updateParticipantTeam');
 
-		// Participants
+		/**
+		 * Participants
+		 */
 		Route::get('/admin/events/{event}/participants', 'Admin\Events\ParticipantsController@index');
 		Route::get('/admin/events/{event}/participants/{participant}', 'Admin\Events\ParticipantsController@show');
 		Route::post('/admin/events/{event}/participants/{participant}', 'Admin\Events\ParticipantsController@update');
 		Route::post('/admin/events/{event}/participants/{participant}/signin', 'Admin\Events\ParticipantsController@signIn');
 
-		// Information
-		Route::post('/admin/events/{event}/information', 'Admin\Events\InformationController@store');
-		Route::post('/admin/information/{information}', 'Admin\Events\InformationController@update');
-		Route::delete('/admin/information/{information}', 'Admin\Events\InformationController@destroy');
-
-		// Annoucements
+		/**
+		 * Annoucements
+		 */
 		Route::post('/admin/events/{event}/annoucements', 'Admin\Events\AnnoucementsController@store');
 		Route::post('/admin/events/{event}/annoucements/{annoucement}', 'Admin\Events\AnnoucementsController@update');
 		Route::delete('/admin/events/{event}/annoucements/{annoucement}', 'Admin\Events\AnnoucementsController@destroy');
 
-		// Tickets & Gifts
+		/**
+		 * Tickets
+		 */
 		Route::get('/admin/events/{event}/tickets', 'Admin\Events\TicketsController@index');
 		Route::post('/admin/events/{event}/tickets', 'Admin\Events\TicketsController@store');
 		Route::get('/admin/events/{event}/tickets/{ticket}', 'Admin\Events\TicketsController@show');
 		Route::post('/admin/events/{event}/tickets/{ticket}', 'Admin\Events\TicketsController@update');
 		Route::delete('/admin/events/{event}/tickets/{ticket}', 'Admin\Events\TicketsController@destroy');
+
+		/**
+		 * Gifts
+		 */
 		Route::post('/admin/events/{event}/freebies/admin', 'Admin\Events\EventsController@freeStaff');
 		Route::post('/admin/events/{event}/freebies/gift', 'Admin\Events\EventsController@freeGift');
 
-		// Sponsors
+		/**
+		 * Sponsors
+		 */
 		Route::post('/admin/events/{event}/sponsors', 'Admin\Events\SponsorsController@store');
 
-		// Venues
+		/**
+		 * Venues
+		 */
 		Route::get('/admin/venues', 'Admin\Events\VenuesController@index');
 		Route::post('/admin/venues', 'Admin\Events\VenuesController@store');
 		Route::get('/admin/venues/{venue}', 'Admin\Events\VenuesController@show');
@@ -160,22 +197,26 @@ Route::group(['middleware' => ['web']], function () {
 		Route::post('/admin/venues/{venue}/{image}', 'Admin\Events\VenuesController@updateImage');
 		Route::delete('/admin/venues/{venue}/{image}', 'Admin\Events\VenuesController@destroyImage');
 		
-		// Gallery
+		/**
+		 * Galleries
+		 */
 		Route::get('/admin/gallery', 'Admin\GalleryController@index');
 		Route::post('/admin/gallery', 'Admin\GalleryController@store');
 		Route::get('/admin/gallery/{album}', 'Admin\GalleryController@show');
 		Route::post('/admin/gallery/{album}', 'Admin\GalleryController@update');
 		Route::delete('/admin/gallery/{album}', 'Admin\GalleryController@destroy');
-
-		// Gallery Images
 		Route::post('/admin/gallery/{album}/upload', 'Admin\GalleryController@uploadImage');
 		Route::post('/admin/gallery/{album}/{image}', 'Admin\GalleryController@updateImage');
 		Route::delete('/admin/gallery/{album}/{image}', 'Admin\GalleryController@destroyImage');
 
-		// Users
+		/**
+		 * Users
+		 */
 		Route::get('/admin/users', 'Admin\UsersController@index');
 
-		// Settings
+		/**
+		 * Settings
+		 */
 		Route::get('/admin/settings', 'Admin\SettingsController@index');
 		Route::post('/admin/settings/', 'Admin\SettingsController@update');
 		Route::post('/admin/settings/generate/qr', 'Admin\SettingsController@regenerateQRCodes');
