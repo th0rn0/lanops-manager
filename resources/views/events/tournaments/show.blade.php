72dd0cb6fa98a98441d4772d9a34ae34ccb24324
Th0rn0 <dev@th0rn0.co.uk>
2018-09-08 13:56:14 +0100

    temp fixes

--- resources/views/events/tournaments/show.blade.php
+++ resources/views/events/tournaments/show.blade.php
@@ -60,7 +60,9 @@
 						<div class="row">
 							<div class="alert alert-success text-center">
 								@foreach ($tournament->challonge_participants as $challonge_participant)
-									<h2>{{ Helpers::getChallongeRankFormat($challonge_participant->final_rank) }} - {{ $tournament->getParticipantByChallongeId($challonge_participant->id)->eventParticipant->user->steamname }}</h2>
+									@if ($tournament->team_size == '1v1')
+										<h2>{{ Helpers::getChallongeRankFormat($challonge_participant->final_rank) }} - {{ $tournament->getParticipantByChallongeId($challonge_participant->id)->eventParticipant->user->steamname }}</h2>
+									@endif
 								@endforeach
 							</div>
 						</div>
