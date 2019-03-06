2763bfb0aaa01ef8da4f772bd615cd7d5c524d9e
Th0rn0 <dev@th0rn0.co.uk>
2019-03-06 19:11:57 +0000

    descriptions

---
+++ src/database/migrations/2019_03_06_190356_add_desc_Settings_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDescSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('settings', function (Blueprint $table) {
+            $table->string('description')->after('value')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('settings', function (Blueprint $table) {
+            //
+        });
+    }
+}
--- src/database/seeds/DatabaseSeeder.php
+++ src/database/seeds/DatabaseSeeder.php
@@ -51,94 +51,107 @@ class DatabaseSeeder extends Seeder
 
         ## Settings
         factory(App\Setting::class)->create([
-            'setting'   => 'org_name',
-            'value'     => env('ORG_NAME', 'OrgNameHere'),
-            'default'   => true,
+            'setting'       => 'org_name',
+            'value'         => env('ORG_NAME', 'OrgNameHere'),
+            'default'       => true,
+            'description'   => 'Name of the Organization'
         ]);
         factory(App\Setting::class)->create([
-            'setting'   => 'org_logo',
-            'value'     => env('ORG_LOGO', '/storage/images/main/logo_main.png'),
-            'default'   => true,
+            'setting'       => 'org_logo',
+            'value'         => env('ORG_LOGO', '/storage/images/main/logo_main.png'),
+            'default'       => true,
+            'description'   => 'Organization Logo'
         ]);
         factory(App\Setting::class)->create([
-            'setting'   => 'org_favicon',
-            'value'     => env('ORG_FAVICON', '/storage/images/main/favicon.ico'),
-            'default'   => true,
+            'setting'       => 'org_favicon',
+            'value'         => env('ORG_FAVICON', '/storage/images/main/favicon.ico'),
+            'default'       => true,
+            'description'   => 'Organization Favicon'
         ]);
         factory(App\Setting::class)->create([
-            'setting'   => 'purchase_terms_and_conditions',
-            'value'     => $faker->paragraph($nbSentences = 90, $variableNbSentences = true),
-            'default'   => true,
+            'setting'       => 'purchase_terms_and_conditions',
+            'value'         => $faker->paragraph($nbSentences = 90, $variableNbSentences = true),
+            'default'       => true,
+            'description'   => 'T&Cs to be displayed on the checkout page'
         ]);
         factory(App\Setting::class)->create([
-            'setting'   => 'registration_terms_and_conditions',
-            'value'     => $faker->paragraph($nbSentences = 90, $variableNbSentences = true),
-            'default'   => true,
+            'setting'       => 'registration_terms_and_conditions',
+            'value'         => $faker->paragraph($nbSentences = 90, $variableNbSentences = true),
+            'default'       => true,
+            'description'   => 'T&Cs to be displayed on the registration page'
         ]);
         factory(App\Setting::class)->create([
-            'setting'   => 'steam_link',
-            'value'     => null,
-            'default'   => true,
+            'setting'       => 'steam_link',
+            'value'         => null,
+            'default'       => true,
+            'description'   => 'Link to your Steam Group'
         ]);
         factory(App\Setting::class)->create([
-            'setting'   => 'teamspeak_link',
-            'value'     => null,
-            'default'   => true,
+            'setting'       => 'teamspeak_link',
+            'value'         => null,
+            'default'       => true,
+            'description'   => 'IP to your Teamspeak Server'
         ]);
         factory(App\Setting::class)->create([
-            'setting'   => 'discord_link',
-            'value'     => null,
-            'default'   => true,
+            'setting'       => 'discord_link',
+            'value'         => null,
+            'default'       => true,
+            'description'   => 'Link to your Discord Server'
         ]);
         factory(App\Setting::class)->create([
-            'setting'   => 'reddit_link',
-            'value'     => null,
-            'default'   => true,
+            'setting'       => 'reddit_link',
+            'value'         => null,
+            'default'       => true,
+            'description'   => 'Link to your Subreddit'
         ]);
         factory(App\Setting::class)->create([
-            'setting'   => 'facebook_link',
-            'value'     => null,
-            'default'   => true,
+            'setting'       => 'facebook_link',
+            'value'         => null,
+            'default'       => true,
+            'description'   => 'Link to your Facebook Page'
         ]);
         factory(App\Setting::class)->create([
-            'setting'   => 'participant_count_offset',
-            'value'     => 0,
-            'default'   => true,
+            'setting'       => 'participant_count_offset',
+            'value'         => 0,
+            'default'       => true,
+            'description'   => 'Increment the Total Participant Count on the Home page'
         ]);
         factory(App\Setting::class)->create([
-            'setting'   => 'lan_count_offset',
-            'value'     => 0,
-            'default'   => true,
+            'setting'       => 'lan_count_offset',
+            'value'         => 0,
+            'default'       => true,
+            'description'   => 'Increment the Total Lan Count on the Home page'
         ]);
         factory(App\Setting::class)->create([
-            'setting'   => 'currency',
-            'value'     => 'GBP',
-            'default'   => true,
+            'setting'       => 'currency',
+            'value'         => 'GBP',
+            'default'       => true,
+            'description'   => 'Currency to use site wide. Only one can be used'
         ]);
         factory(App\Setting::class)->create([
-            'setting'   => 'about_main',
-            'value'     => $faker->paragraph($nbSentences = 90, $variableNbSentences = true),
-            'default'   => true,
+            'setting'       => 'about_main',
+            'value'         => $faker->paragraph($nbSentences = 90, $variableNbSentences = true),
+            'default'       => true,
         ]);
         factory(App\Setting::class)->create([
-            'setting'   => 'about_short',
-            'value'     => $faker->paragraph($nbSentences = 4, $variableNbSentences = true),
-            'default'   => true,
+            'setting'       => 'about_short',
+            'value'         => $faker->paragraph($nbSentences = 4, $variableNbSentences = true),
+            'default'       => true,
         ]);
         factory(App\Setting::class)->create([
-            'setting'   => 'about_our_aim',
-            'value'     => $faker->paragraph($nbSentences = 90, $variableNbSentences = true),
-            'default'   => true,
+            'setting'       => 'about_our_aim',
+            'value'         => $faker->paragraph($nbSentences = 90, $variableNbSentences = true),
+            'default'       => true,
         ]);
         factory(App\Setting::class)->create([
-            'setting'   => 'about_who',
-            'value'     => $faker->paragraph($nbSentences = 90, $variableNbSentences = true),
-            'default'   => true,
+            'setting'       => 'about_who',
+            'value'         => $faker->paragraph($nbSentences = 90, $variableNbSentences = true),
+            'default'       => true,
         ]);
         factory(App\Setting::class)->create([
-            'setting'   => 'social_facebook_page_access_token',
-            'value'     => $null,
-            'default'   => true,
+            'setting'       => 'social_facebook_page_access_token',
+            'value'         => $null,
+            'default'       => true,
         ]);
 
         factory(App\Game::class)->create([
