6954a326befe1fcd9d4fce207fc038c6674dc012
Th0rn0 <dev@th0rn0.co.uk>
2019-03-09 13:00:20 +0000

    comment blade

--- src/resources/views/news/show.blade.php
+++ src/resources/views/news/show.blade.php
@@ -21,22 +21,7 @@
 				</h3> 
 			</div>
 			@foreach ($news_article->comments->reverse() as $comment)
-				<div class="row">
-					<div class="col-xs-2">
-						<img class="img-responsive img-rounded img-small news-post-comment-image" src="{{ $comment->user->avatar }}"/>
-						<p class="news-post-comment-image-text">{{ $comment->user->steamname }}</p>
-					</div>
-					<div class="col-xs-10">
-						<p>{{ $comment->comment }}</p>
-						<span class="text-muted"><small>Posted on: {{ $comment->created_at }}</small></span>
-						@if (Auth::user() && Auth::user()->getAdmin())
-							{{ Form::open(array('url'=>'/admin/news/' . $news_article->slug . '/comments/' . $comment->id)) }}
-								{{ Form::hidden('_method', 'DELETE') }}
-								<button type="submit" class="btn btn-sm btn-danger">Delete Comment</button>
-							{{ Form::close() }}
-						@endif
-					</div>
-				</div>
+				@include ('layouts._partials._news.comment')
 				<hr>
 				<br>
 			@endforeach
