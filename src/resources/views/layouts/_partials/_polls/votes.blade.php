dd2a4d67669190052ae13e9d00667ca963ae07d8
Th0rn0 <th0rn0@lanops.co.uk>
2019-05-05 14:32:15 +0100

    fixed poll display

--- src/resources/views/layouts/_partials/_polls/votes.blade.php
+++ src/resources/views/layouts/_partials/_polls/votes.blade.php
@@ -1,38 +1,42 @@
 <div class="row">
-		<div class="col-xs-12 col-sm-6 col-md-9">
+		<div class="col-xs-12 col-sm-9 col-md-9">
 			<table width="100%" class="table table-striped table-hover">
 				@foreach ($poll->options->reverse() as $option)
 					<tr class="table-row odd gradeX">
-						@if (!$poll->hasEnded())
-							<td width="15%">
-								@if (Auth::user() && !$option->hasVoted())
-									<a href="/polls/{{ $poll->slug }}/options/{{ $option->id }}/vote">
-										<button type="button" class="btn btn-default btn-sm btn-block">Vote</button>
-									</a>
-								@elseif (Auth::user() && $option->hasVoted())
-									<a href="/polls/{{ $poll->slug }}/options/{{ $option->id }}/abstain">
-										<button type="button" class="btn btn-default btn-sm btn-block">Remove</button>
-									</a>
-								@endif
-							</td>
-						@endif
 						<td width="30%">{{ $option->name }}</td>
-						<td width="40%">
+						<td>
 							<div class="progress-bar" role="progressbar" aria-valuenow="{{ number_format($option->getPercentage(), 2) }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ $option->getPercentage() }}%;">
 								{{ $option->getTotalVotes() }}
 							</div>
 						</td>
-						<td width="10%">{{ $option->getTotalVotes() }} Votes</td>
+						@if (!$poll->hasEnded() && Auth::user())
+							<td width="10%">
+								@if (Auth::user() && !$option->hasVoted())
+									<a href="/polls/{{ $poll->slug }}/options/{{ $option->id }}/vote">
+										<button type="button" class="btn btn-success btn-sm btn-block">Vote</button>
+									</a>
+								@elseif (Auth::user() && $option->hasVoted())
+									<a href="/polls/{{ $poll->slug }}/options/{{ $option->id }}/abstain">
+										<button type="button" class="btn btn-danger btn-sm btn-block">Remove</button>
+									</a>
+								@endif
+							</td>
+						@endif
 					</tr>
 				@endforeach
 			</table>
 		</div>
-		<div class="col-xs-12 col-sm-6 col-md-3">
+		<div class="col-xs-12 col-sm-3 col-md-3">
 			@if (Auth::user())
 				{{ Form::label('name','You have voted for...',array('id'=>'','class'=>'')) }}
 				@foreach ($poll->options as $option)
 					@if ($option->hasVoted())
-						<p>{{ $option->name }}</p>
+						<p>
+							<a href="/polls/{{ $poll->slug }}/options/{{ $option->id }}/abstain">
+								<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+							</a>
+							{{ $option->name }}
+						</p>
 					@endif
 				@endforeach
 				@if (!$poll->hasEnded() && $poll->allow_options_user)
