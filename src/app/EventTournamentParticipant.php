6992a32d8ebf60160658474c87ef1103701d1f9f
Th0rn0 <Th0rn0@lanops.co.uk>
2019-06-16 17:58:28 +0100

    cleanup

--- src/app/EventTournamentParticipant.php
+++ src/app/EventTournamentParticipant.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use Settings;
+
 use Illuminate\Database\Eloquent\Model;
 
 use GuzzleHttp\Client;
@@ -43,6 +45,11 @@ class EventTournamentParticipant extends Model
     {
         parent::boot();
         self::created(function ($model) {
+            if (Settings::isCreditEnabled()) {
+                if (Settings::getCreditTournamentParticipation() != 0 || Settings::getCreditTournamentParticipation() != null) {
+                    $model->user->editCredit(-1 * abs(Settings::getCreditTournamentParticipation()), false, 'Tournament Registration');
+                }
+            }
             if ((!isset($model->event_tournament_team_id) || trim($model->event_tournament_team_id) == '') &&
                 (!$model->pug && $model->event_tournament_team_id == null) &&
                 $model->eventTournament->format != 'list'
@@ -62,6 +69,11 @@ class EventTournamentParticipant extends Model
             return true;
         });
         self::deleting(function ($model) {
+            if (Settings::isCreditEnabled()) {
+                if (Settings::getCreditTournamentParticipation() != 0 || Settings::getCreditTournamentParticipation() != null) {
+                    $model->user->editCredit(-1 * abs(Settings::getCreditTournamentParticipation()), false, 'Tournament De-Registration');
+                }
+            }
             if (!$model->pug && $model->event_tournament_team_id == null && $model->eventTournament->format != 'list') {
                 $challonge = new Challonge(config('challonge.api_key'));
                 $participant = $challonge->getParticipant(
