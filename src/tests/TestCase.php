2948f7a6cd1491c12fb8128672ac687fcf111228
Th0rn0 <Th0rn0@lanops.co.uk>
2019-09-02 22:53:07 +0100

    drone update

--- src/tests/TestCase.php
+++ src/tests/TestCase.php
@@ -6,13 +6,5 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    /**
-     * Creates the application.
-     *
-     * @return \Laravel\Lumen\Application
-     */
-    public function createApplication()
-    {
-        return include __DIR__ . '/../bootstrap/app.php';
-    }
+    use CreatesApplication;
 }
