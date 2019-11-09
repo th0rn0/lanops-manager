752f2a90a779a8aa2f229a54dec192d764af340e
Thornton Phillis <dev@th0rn0.co.uk>
2019-11-09 16:10:37 +0000

    Hotfix/css editor (#61)
    * fixing
    
    * more indepth CSS variables
    
    * more editing options
    
    * cleared up admin appearance page

--- src/database/seeds/AppearanceTableSeeder.php
+++ src/database/seeds/AppearanceTableSeeder.php
@@ -25,21 +25,52 @@ class AppearanceTableSeeder extends Seeder
         ]);
 
         factory(App\Appearance::class)->create([
+            'key'   => 'color_primary_text',
+            'value' => 'white',
+            'type'  => 'CSS_VAR',
+        ]);
+
+        factory(App\Appearance::class)->create([
             'key'   => 'color_secondary',
             'value' => '#333',
             'type'  => 'CSS_VAR',
         ]);
 
         factory(App\Appearance::class)->create([
-            'key'   => 'color_links',
-            'value' => 'blue',
+            'key'   => 'color_secondary_text',
+            'value' => 'white',
             'type'  => 'CSS_VAR',
         ]);
 
         factory(App\Appearance::class)->create([
-            'key'   => 'color_background',
-            'value' => 'white',
+            'key'   => 'color_body_links',
+            'value' => 'orange',
             'type'  => 'CSS_VAR',
         ]);
+
+        factory(App\Appearance::class)->create([
+            'key'   => 'color_body_background',
+            'value' => '#fff',
+            'type'  => 'CSS_VAR',
+        ]);
+
+        factory(App\Appearance::class)->create([
+            'key'   => 'color_body_text',
+            'value' => '#333',
+            'type'  => 'CSS_VAR',
+        ]);
+
+        factory(App\Appearance::class)->create([
+            'key'   => 'color_header_background',
+            'value' => '#333',
+            'type'  => 'CSS_VAR',
+        ]);
+
+        factory(App\Appearance::class)->create([
+            'key'   => 'color_header_text',
+            'value' => '#9d9d9d',
+            'type'  => 'CSS_VAR',
+        ]);
+
     }
 }
