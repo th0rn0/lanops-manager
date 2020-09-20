53c6a0f25fcb3de4c8d063c440022956c29d97fa
Hahn, Andreas <andyhahn91@gmail.com>
2020-09-20 20:37:26 +0200

    TryUpdate FontAwesome

--- src/resources/views/layouts/admin-default.blade.php
+++ src/resources/views/layouts/admin-default.blade.php
@@ -21,7 +21,6 @@
 		<link href="/css/admin.css" rel="stylesheet">
 
 		<!-- Custom Fonts -->
-		<link href="/fonts/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
 		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
