da2c0016d6dc8e8541417bad881367778c20e85a
Alexander Volz <Alexander.volz@nagold.de>
2020-09-21 18:14:01 +0200

    test languagedir check

--- src/app/Http/Middleware/LanguageSwitcher.php
+++ src/app/Http/Middleware/LanguageSwitcher.php
@@ -22,6 +22,8 @@ class LanguageSwitcher
         if ($locale = Setting::getSiteLocale())
         {
             $locale_dirs = array_filter(glob('../resources/lang/*'), 'is_dir');
+            print_r(glob('../resources/lang/*'));
+            print_r($locale_dirs);
             if(in_array($locale, $locale_dirs))
             {
                 App::setLocale($locale);
