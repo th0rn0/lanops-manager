fc4783bbb898bc15b56415dc4b3e6167aaf1f098
Hahn, Andreas <andyhahn91@gmail.com>
2020-09-21 23:11:04 +0200

    Translate about

--- src/resources/views/about.blade.php
+++ src/resources/views/about.blade.php
@@ -7,18 +7,18 @@
 <div class="container">
 
 	<div class="page-header">
-		<h1>About Us</h1> 
+		<h1>@lang('about.aboutus')</h1> 
 	</div>
 	<div class="page-header">
-		<h3>Who We Are</h3> 
+		<h3>@lang('about.whoeweare')</h3> 
 	</div>
 	{!! Settings::getAboutMain() !!}
 	<div class="page-header">
-		<h3>Our Aim</h3> 
+		<h3>@lang('about.ouraim')</h3> 
 	</div>
 	{!! Settings::getAboutOurAim() !!}
 	<div class="page-header">
-		<h3>The Who's Who</h3> 
+		<h3>@lang('about.whoswho')</h3> 
 	</div>
 	{!! Settings::getAboutWho() !!}
 
