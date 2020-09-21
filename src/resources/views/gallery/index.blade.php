5f5e46e4124f88cc0f0b37698c45ca4be2eeb6b1
Alexander Volz <Alexander.volz@nagold.de>
2020-09-22 00:04:50 +0200

    updating translation of gallery views

--- src/resources/views/gallery/index.blade.php
+++ src/resources/views/gallery/index.blade.php
@@ -1,20 +1,20 @@
 @extends ('layouts.default')
 
-@section ('page_title', Settings::getOrgName() . ' Gallery')
+@section ('page_title', Settings::getOrgName() . ' - ' . __('gallery.gallery'))
 
 @section ('content')
 
 <div class="container">
 
 	<div class="page-header">
-		<h1>Gallery</h1> 
+		<h1>@lang('gallery.gallery')</h1> 
 	</div>
 	<div class="row">
 		@foreach ($albums as $album)
 			<div class="well well-sm col-sm-3 col-xs-6">
 				<h4>{{ $album->name }}</h4>
 				@if ($album->event)
-					<h5>Event: {{ $album->event->display_name }}</h5>
+					<h5>@lang('gallery.event') {{ $album->event->display_name }}</h5>
 				@endif
 				<p>{{ $album->description }}</p>
 				<a href="/gallery/{{ $album->slug }}">
--- src/resources/views/gallery/show.blade.php
+++ src/resources/views/gallery/show.blade.php
@@ -1,6 +1,6 @@
 @extends ('layouts.default')
 
-@section ('page_title', Settings::getOrgName() . ' Gallery')
+@section ('page_title', Settings::getOrgName() . ' - ' . __('gallery.gallery'))
 
 @section ('content')
 
