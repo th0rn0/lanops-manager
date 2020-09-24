cc07710e3439ea7732cae11b0908e7bce21538cb
Alexander Volz <Alexander.volz@nagold.de>
2020-09-25 00:16:22 +0200

    fix cookieconsent styling, localisation and content

---
+++ src/resources/lang/de/cookieconsent.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | cookie consent
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'message1' => 'Diese Seite verwendet Cookies um das Nutzererlebnis zu steigern. Weitere Informationen findest du auf unserer',
+    'agree' => 'Akzeptieren',
+
+
+];
