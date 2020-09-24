a78fbb0f0bc113cf2ab9c32bec8f6ff5840ec3a7
Alexander Volz <Alexander.volz@nagold.de>
2020-09-25 00:30:11 +0200

    fix cookieconsent styling, localisation and content

---
+++ src/resources/views/vendor/cookieConsent/dialogContents.blade.php
@@ -0,0 +1,12 @@
+<div class="js-cookie-consent cookie-consent">
+
+    <span class="cookie-consent__message">
+        <h3>@lang('cookieconsent.message') <a href="/legalnotice">@lang('layouts.default_legal_and_privacy')</a>.</h3>
+    </span>
+
+    <button class="js-cookie-consent-agree cookie-consent__agree">
+        @lang('cookieconsent.agree')
+    </button>
+
+
+</div>
