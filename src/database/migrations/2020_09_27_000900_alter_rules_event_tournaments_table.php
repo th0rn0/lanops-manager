d775b2a742e335be6a80b6142442a4f5f859daf7
Hahn, Andreas <andyhahn91@gmail.com>
2020-09-27 00:16:23 +0200

    Alter EventTournaments to store longtext instead of string

---
+++ src/database/migrations/2020_09_27_000900_alter_rules_event_tournaments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRulesEventTournamentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('event_tournaments', function (Blueprint $table) {
+            $table->longText('rules')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('event_tournaments', function (Blueprint $table) {
+            $table->string('rules')->change();
+        });
+    }
+}
