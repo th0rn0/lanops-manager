97fc2012891f2f1d462645623aa1a7c92e40fc66
Alexander Volz <Alexander.volz@nagold.de>
2020-09-27 16:36:16 +0200

    fixed twitter share link

--- src/resources/views/layouts/_partials/_news/short.blade.php
+++ src/resources/views/layouts/_partials/_news/short.blade.php
@@ -13,7 +13,7 @@
 					<a href="https://www.facebook.com/sharer/sharer.php?u={{ url('/news') }}/{{ $newsArticle->slug }}&t={{ $newsArticle->title }}" target="_blank">
 						<i class="fab fa-facebook-f"></i>	
 					</a>
-					<a href="http://twitter.com/share?text={{ $newsArticle->title }}&url=http://{{ url('/news') }}/{{ $newsArticle->slug }}&hashtags={{ $newsArticle->getTags(',') }}" target="_blank">
+					<a href="http://twitter.com/share?text={{ $newsArticle->title }}&url={{ url('/news') }}/{{ $newsArticle->slug }}&hashtags={{ $newsArticle->getTags(',') }}" target="_blank">
 						<i class="fab fa-twitter"></i>
 					</a>
 				</div>
