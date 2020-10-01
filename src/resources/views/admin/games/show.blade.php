4560d99389c0e8d7fc02618bba710db4eba4b697
Hahn, Andreas <andyhahn91@gmail.com>
2020-10-01 23:50:13 +0200

    Fix Typo

--- src/resources/views/admin/games/show.blade.php
+++ src/resources/views/admin/games/show.blade.php
@@ -237,7 +237,7 @@
 						</div>
 						<div class="form-group">
 						{{ Form::label('gamecommandhandler','Game Commandhandler',array('id'=>'','class'=>'')) }}
-						{{ Form::select('gamecommandhandler', Helpers::getGameCommandHandler(), $game->gamecommandhandler, array('id'=>'gamecommandhandler','class'=>'form-control')) }}
+						{{ Form::select('gamecommandhandler', Helper::getGameCommandHandler(), $game->gamecommandhandler, array('id'=>'gamecommandhandler','class'=>'form-control')) }}
 						</div>
 						<div class="form-group">
 							@if ($game->image_thumbnail_path != '')
