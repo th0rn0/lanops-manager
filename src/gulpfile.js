ce71fd4ef647c4d0aaf9ca404b8a0223736ecd2d
Hahn, Andreas <andyhahn91@gmail.com>
2020-10-03 00:26:42 +0200

    Update Elixir to Mixin

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,17 +1,3 @@
-var elixir = require('laravel-elixir');
-elixir.config.assetsDir = 'resources/assets/';
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
-
-elixir(function(mix) {
-    mix.sass(['app.scss'], 'public/css/app.css');
-    mix.sass(['admin.scss'], 'public/css/admin.css');
-});
+var mix = require('laravel-mixin');
+mix.sass('resources/assets/sass/app.scss', 'public/css')
+    .sass('resources/assets/sass/admin.scss', 'public/css/admin');
