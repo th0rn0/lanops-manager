ab3082700a9f4344af1691f5b1be5a89c81f516b
Hahn, Andreas <andyhahn91@gmail.com>
2020-10-02 21:35:31 +0200

    Lan::getFromJson -> Lang::get

--- src/app/Notifications/ResetPassword.php
+++ src/app/Notifications/ResetPassword.php
@@ -59,12 +59,12 @@ class ResetPassword extends Notification
         }
 
         return (new MailMessage)
-            ->from(config('mail.from.address', Settings::getOrgName())
-            ->subject(Lang::getFromJson('Reset Password Notification'))
-            ->line(Lang::getFromJson('You are receiving this email because we received a password reset request for your account.'))
-            ->action(Lang::getFromJson('Reset Password'), url(config('app.url').route('password.reset', ['token' => $this->token, 'email' => $notifiable->getEmailForPasswordReset()], false)))
-            ->line(Lang::getFromJson('This password reset link will expire in :count minutes.', ['count' => config('auth.passwords.users.expire')]))
-            ->line(Lang::getFromJson('If you did not request a password reset, no further action is required.'));
+            ->from(config('mail.from.address', Settings::getOrgName()))
+            ->subject(Lang::get('Reset Password Notification'))
+            ->line(Lang::get('You are receiving this email because we received a password reset request for your account.'))
+            ->action(Lang::get('Reset Password'), url(config('app.url').route('password.reset', ['token' => $this->token, 'email' => $notifiable->getEmailForPasswordReset()], false)))
+            ->line(Lang::get('This password reset link will expire in :count minutes.', ['count' => config('auth.passwords.users.expire')]))
+            ->line(Lang::get('If you did not request a password reset, no further action is required.'));
     }
 
     /**
