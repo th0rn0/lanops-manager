1c8c9ef2281a7d29320dc8d48a93c66ccfa829de
Hahn, Andreas <andyhahn91@gmail.com>
2020-10-03 01:40:36 +0200

    use let

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -1,4 +1,4 @@
-const { mix } = require('laravel-mix');
+let mix = require('laravel-mix');
 
 mix.sass('resources/assets/sass/app.scss', 'public/css')
     .sass('resources/assets/sass/admin.scss', 'public/css/admin');
