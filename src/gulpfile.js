eb8175ff2e8505163c3683cc62493e3d60f1181b
Hahn, Andreas <andyhahn91@gmail.com>
2020-10-03 00:56:49 +0200

    Fix require naming

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,3 +1,3 @@
-var mix = require('laravel-mixin');
+var mix = require('laravel-mix');
 mix.sass('resources/assets/sass/app.scss', 'public/css')
     .sass('resources/assets/sass/admin.scss', 'public/css/admin');
