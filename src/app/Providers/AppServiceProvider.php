facb410f64ff96c9f37c93be36504124c05c5b75
Hahn, Andreas <andyhahn91@gmail.com>
2020-10-02 23:18:25 +0200

    Update app register migration generator

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -111,7 +111,7 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         if (config('app.debug') === true) {
-            // $this->app->register(\Way\Generators\GeneratorsServiceProvider::class);
+            $this->app->register(\KitLoong\MigrationsGenerator\MigrationsGeneratorServiceProvider::class);
             $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
             $this->app->register(\Orangehill\Iseed\IseedServiceProvider::class);
         }
