fe006766fbc7f91e2cc0a0d7706fa2380be16bf8
Alexander Volz <Alexander.volz@nagold.de>
2020-10-03 14:29:15 +0200

    fixed events lists output to display html

--- src/resources/views/events/index.blade.php
+++ src/resources/views/events/index.blade.php
@@ -16,8 +16,8 @@
 				<strong><a href="/events/{{ $event->slug }}">{{ $event->display_name }}</a></strong>
 			</div>
 			<div class="panel-body">
-				<h5>{{!! $event->desc_short !!}}</h5>
-				<p>{{!! $event->essential_info !!}}</p>
+				<h5>{!! $event->desc_short !!}</h5>
+				<p>{!! $event->essential_info !!}</p>
 				<p class="bg-success  padding">@lang('events.start'): {{ date('H:i d-m-Y', strtotime($event->start)) }}</p>
 				<p class="bg-danger  padding">@lang('events.end'): {{ date('H:i d-m-Y', strtotime($event->end)) }}</p>
 				<p class="bg-info  padding">@lang('events.seatingcapacity'): {{ $event->getSeatingCapacity() }}</p>
