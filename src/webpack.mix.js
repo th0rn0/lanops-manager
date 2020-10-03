a169373d7d86ad42017a6aa9b22602b93e757761
Hahn, Andreas <andyhahn91@gmail.com>
2020-10-03 02:05:23 +0200

    use bootstrap from nodemodules

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -1,4 +1,4 @@
 let mix = require('laravel-mix');
 
 mix.sass('resources/assets/sass/app.scss', 'public/css')
-    .sass('resources/assets/sass/admin.scss', 'public/css/admin');
+    .sass('resources/assets/sass/admin.scss', 'public/css');
