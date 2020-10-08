47a9150b73bb8445bea5cd1687b22cc70e121a37
Hahn, Andreas <andyhahn91@gmail.com>
2020-10-08 19:41:11 +0200

    Add copyToClipBoard script

--- src/resources/views/events/home.blade.php
+++ src/resources/views/events/home.blade.php
@@ -202,6 +202,21 @@
 						</div>
 					@endforeach
 				</div>
+
+				<script>
+					function copyToClipBoard(inputId) {
+						/* Get the text field */
+						var copyText = document.getElementById(inputId);
+				
+						/* Select the text field */
+						copyText.select();
+						copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+				
+						/* Copy the text inside the text field */
+						document.execCommand("copy");
+					}
+				</script>
+				
 			@endif
 	@endif				
 
