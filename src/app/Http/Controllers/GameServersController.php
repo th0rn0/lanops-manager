86ee114fa851ef5aa6792b9118a9c67573e4449e
Hahn, Andreas <andyhahn91@gmail.com>
2020-10-09 22:21:16 +0200

    request GetCurrentChallengeInfo

--- src/app/Http/Controllers/GameServersController.php
+++ src/app/Http/Controllers/GameServersController.php
@@ -59,12 +59,11 @@ class GameServersController extends Controller
             if ($game->gamecommandhandler == 2) {
                 try {
                     $maniaConnection = new Connection($gameServer->address, $gameServer->rcon_port, 5, "SuperAdmin", $gameServer->rcon_password, Connection::API_2011_02_21);
-                    $result->info = $maniaConnection->getGameInfos();
+                    $result->info = $maniaConnection->execute("GetCurrentChallengeInfo", array());
                     $result->players = $maniaConnection->getPlayerList();
                 } catch (Exception $e) {
                     $result->error = $e->getMessage();
-                }
-            }
+                }G            }
         }
 
         return json_encode($result);
