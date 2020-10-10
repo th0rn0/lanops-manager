4ca10cb9256a6bc90ccc78007c74a277b180b936
Alexander Volz <Alexander.volz@nagold.de>
2020-10-11 00:59:54 +0200

    fixed help view

--- src/resources/views/help/show.blade.php
+++ src/resources/views/help/show.blade.php
@@ -13,15 +13,25 @@
 		@endif
 	</div>
 	
-	
+	<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
 		@foreach ($helpCategory->entrys as $entry)
 
-					{{ $entry->name }}
-					
+					<div class="panel panel-default">
+						<div class="panel-heading" role="tab" id="headingOne">
+							<h4 class="panel-title">
+								<a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse_{{ $entry->id }}" aria-expanded="true" aria-controls="collapse_{{ $entry->id }}">
+									{{ $entry->name }}
+								</a>
+							</h4>
+						</div>
+						<div id="collapse_{{ $entry->id }}" class="panel-collapse collapse" role="tabpanel" aria-labelledby="collaspe_{{ $entry->id }}">
+							<div class="panel-body">
 								{!! $entry->content !!}
-			
+							</div>
+						</div>
+					</div>
 		@endforeach
-
+	</div>
 
 </div>
 
