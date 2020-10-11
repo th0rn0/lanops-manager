9fd3b7633e33af15fcfe54f2ce35eb9a9f05fafe
Hahn, Andreas <andyhahn91@gmail.com>
2020-10-11 03:22:18 +0200

    Fix Help

--- src/resources/views/help/show.blade.php
+++ src/resources/views/help/show.blade.php
@@ -14,24 +14,22 @@
 	</div>
 	
 <div class="panel-group" id="helpcategory_{{ $helpCategory->slug }}" role="tablist" aria-multiselectable="false">
-		@foreach ($helpCategory->entrys as $entry)
-			<div class="panel panel-default">
-				<div class="panel-heading" role="tab" id="{{ $entry->nice_name }}">
-					<h4 class="panel-title">
-						<a role="button" data-toggle="collapse" class="accordion-toggle accordion-arrow-toggle" data-parent="#helpcategory_{{ $helpCategory->slug }}" href="#collapse_{{ $entry->nice_name }}" aria-expanded="false" aria-controls="collapse_{{ $entry->nice_name }}">
-							{{ $entry->display_name }}
-						</a>
-					</h4>
-				</div>
-				<div id="collapse_{{ $entry->nice_name }}" class="panel-collapse collapse" role="tabpanel" aria-labelledby="{{ $entry->nice_name }}">
-					<div class="panel-body">
-						{!! $entry->content !!}
-					</div>
+	@foreach ($helpCategory->entrys as $entry)
+		<div class="panel panel-default">
+			<div class="panel-heading" role="tab" id="{{ $entry->nice_name }}">
+				<h4 class="panel-title">
+					<a role="button" data-toggle="collapse" class="accordion-toggle accordion-arrow-toggle" data-parent="#helpcategory_{{ $helpCategory->slug }}" href="#collapse_{{ $entry->nice_name }}" aria-expanded="false" aria-controls="collapse_{{ $entry->nice_name }}">
+						{{ $entry->display_name }}
+					</a>
+				</h4>
+			</div>
+			<div id="collapse_{{ $entry->nice_name }}" class="panel-collapse collapse" role="tabpanel" aria-labelledby="{{ $entry->nice_name }}">
+				<div class="panel-body">
+					{!! $entry->content !!}
 				</div>
 			</div>
-		@endforeach
-	</div>
-
+		</div>
+	@endforeach
 </div>
 
 @endsection
