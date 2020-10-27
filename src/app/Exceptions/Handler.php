05a41a96c1e23373676c013e86bba359a53f4b32
Alexander Volz <Alexander.volz@nagold.de>
2020-10-27 22:25:31 +0100

    fixed exceptionhandler, settingstableseeder

--- src/app/Exceptions/Handler.php
+++ src/app/Exceptions/Handler.php
@@ -1,8 +1,9 @@
 <?php
+
 namespace App\Exceptions;
-use Throwable;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+
 class Handler extends ExceptionHandler
 {
     /**
@@ -13,6 +14,7 @@ class Handler extends ExceptionHandler
     protected $dontReport = [
         //
     ];
+
     /**
      * A list of the inputs that are never flashed for validation exceptions.
      *
@@ -22,25 +24,14 @@ class Handler extends ExceptionHandler
         'password',
         'password_confirmation',
     ];
+
     /**
-     * Report or log an exception.
+     * Register the exception handling callbacks for the application.
      *
-     * @param  \      $exception
      * @return void
      */
-    public function report(Throwable $exception)
+    public function register()
     {
-        parent::report($exception);
-    }
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Throwable  $exception
-     * @return \Illuminate\Http\Response
-     */
-    public function render($request, Throwable $exception)
-    {
-        return parent::render($request, $exception);
+        //
     }
 }
--- src/database/seeders/SettingsTableSeeder.php
+++ src/database/seeders/SettingsTableSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Seteting;
+use App\Setting;
 use Faker\Factory as Faker;
 
 class SettingsTableSeeder extends Seeder
