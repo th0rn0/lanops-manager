d08410f316fb6ec82363f5aafffd225197bd6fac
Alexander Volz <Alexander.volz@nagold.de>
2020-10-28 09:33:55 +0100

    fixed stringlength in poll description

---
+++ src/database/migrations/2020_10_28_0852_edit_description_polls_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class EditDescriptionPollsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('polls', function (Blueprint $table) {
+            $table->string('description', 4294967295)->nullable()->after('slug')->change();            ;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('polls', function (Blueprint $table) {
+            $table->dropColumn('description');
+        });
+    }
+}
