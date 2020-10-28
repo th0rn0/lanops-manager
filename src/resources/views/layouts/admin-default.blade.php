11f03df307deb3d7a4f1d3732b270d798f83927d
Alexander Volz <Alexander.volz@nagold.de>
2020-10-28 18:53:57 +0100

    fixed error flashing in admin menu

--- src/resources/views/layouts/admin-default.blade.php
+++ src/resources/views/layouts/admin-default.blade.php
@@ -90,6 +90,15 @@
 								</p>
 							@endif
 						@endforeach
+						@if ($errors->any())
+							<div class="alert alert-danger">
+								<ul>
+									@foreach ($errors->all() as $error)
+										<li>{{ $error }}</li>
+									@endforeach
+								</ul>
+							</div>
+						@endif
 					</div>
 					@yield('content')
 				</div>
