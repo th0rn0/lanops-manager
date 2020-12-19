e88002664823fea6fd831c2d035cb330e261d5dd
Alexander Volz <Alexander.volz@nagold.de>
2020-12-19 02:21:19 +0100

    Fixed Mailtemplate when no event is planned

--- src/app/Mail/EventulaMailingMail.php
+++ src/app/Mail/EventulaMailingMail.php
@@ -64,26 +64,30 @@ class EventulaMailingMail extends TemplateMailable
     
 
 
-    public function __construct(User $user, Event $nextevent)
+    public function __construct(User $user, ?Event $nextevent)
     {
         $this->firstname = $user->firstname;
         $this->surname = $user->surname;
         $this->email = $user->email;
         $this->username = $user->username_nice;
-        $this->nextevent_display_name = $nextevent->display_name;
-        $this->nextevent_capacity = $nextevent->capacity;
-        $this->nextevent_desc_long = $nextevent->desc_long;
-        $this->nextevent_desc_short = $nextevent->desc_short;
-        $this->nextevent_essential_info = $nextevent->essential_info;
-        $this->nextevent_start = $nextevent->start;
-        $this->nextevent_end = $nextevent->end;
-        $this->nextevent_venue_display_name = $nextevent->venue->display_name;
-        $this->nextevent_venue_address_1 = $nextevent->venue->address_1;
-        $this->nextevent_venue_address_2 = $nextevent->venue->address_2;
-        $this->nextevent_venue_address_street = $nextevent->venue->address_street;
-        $this->nextevent_venue_address_city = $nextevent->venue->address_city;
-        $this->nextevent_venue_address_postcode = $nextevent->venue->address_postcode;
-        $this->nextevent_venue_address_country = $nextevent->venue->address_country;
+
+        if (isset($nextevent)) 
+        {
+            $this->nextevent_display_name = $nextevent->display_name;
+            $this->nextevent_capacity = $nextevent->capacity;
+            $this->nextevent_desc_long = $nextevent->desc_long;
+            $this->nextevent_desc_short = $nextevent->desc_short;
+            $this->nextevent_essential_info = $nextevent->essential_info;
+            $this->nextevent_start = $nextevent->start;
+            $this->nextevent_end = $nextevent->end;
+            $this->nextevent_venue_display_name = $nextevent->venue->display_name;
+            $this->nextevent_venue_address_1 = $nextevent->venue->address_1;
+            $this->nextevent_venue_address_2 = $nextevent->venue->address_2;
+            $this->nextevent_venue_address_street = $nextevent->venue->address_street;
+            $this->nextevent_venue_address_city = $nextevent->venue->address_city;
+            $this->nextevent_venue_address_postcode = $nextevent->venue->address_postcode;
+            $this->nextevent_venue_address_country = $nextevent->venue->address_country;
+        }
 
 
     }
