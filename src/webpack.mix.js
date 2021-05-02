3fd58bd7c95655992062992dcba4bd5c3a01e8d7
Alexander Volz <Alexander@volzit.de>
2021-05-02 02:48:40 +0200

    updated laravel mix to 6x

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -1,5 +1,6 @@
 let mix = require('laravel-mix');
 require('laravel-mix-purgecss');
+var path = require('path');
 
 mix.sass('resources/assets/sass/app.scss', 'public/css')
     .sass('resources/assets/sass/admin.scss', 'public/css').options({
