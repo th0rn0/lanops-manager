473ddff5a7bd659ac1b6c5c8809ef7d5caa9f220
Apfelwurm <wwwmasterwww@gmail.com>
2021-10-05 01:45:53 +0200

    updated comments

--- src/app/Http/Controllers/Admin/PurchasesController.php
+++ src/app/Http/Controllers/Admin/PurchasesController.php
@@ -58,7 +58,7 @@ class PurchasesController extends Controller
             ->withPurchase($purchase);
     } 
     /**
-     * Show Purchase Page
+     * Set Purchase Success
      * @param Purchase $purchase
      * @return View
      */
