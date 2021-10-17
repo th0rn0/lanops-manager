1dd90b0ccd4d3b4cf988a60ada899201bf5131a7
Andreas Hahn <andyhahn91@gmail.com>
2021-10-17 23:10:52 +0200

    Add 404 API Message Fix #188

--- src/app/Exceptions/Handler.php
+++ src/app/Exceptions/Handler.php
@@ -2,6 +2,8 @@
 
 namespace App\Exceptions;
 
+use Throwable;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
@@ -33,5 +35,26 @@ class Handler extends ExceptionHandler
     public function register()
     {
         //
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Throwable $exception
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @throws \Exception
+     */
+    public function render($request, Throwable $exception)
+    {
+        if (
+            strpos($request->getRequestUri(), '/api/', 0) === 0 &&
+            $exception instanceof NotFoundHttpException
+        ) {
+            return response()->json(['error' => 'Request URI Not found'], 404);
+        }
+
+        return parent::render($request, $exception);
     }
 }
