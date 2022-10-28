e4142702282b1f281b5867b171964d20c558b4d0
Kevin Als <kevin@tideo.dk>
2022-10-28 12:38:00 +0200

    Added translation using Weblate (Danish)

---
+++ src/lang/da/cookieconsent.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | cookie consent
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'message' => 'Your experience on this site will be improved by allowing cookies. You can find more information on',
+    'agree' => 'Agree',
+];
