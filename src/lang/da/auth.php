11f29b402a247f89ea49eca87005327b9072c364
Kevin Als <kevin@tideo.dk>
2022-10-28 12:33:52 +0200

    Added translation using Weblate (Danish)

---
+++ src/lang/da/auth.php
@@ -0,0 +1,44 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'failed' => 'These credentials do not match our records.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'login_to_continue' => 'Login to continue',
+    'login_methods' => 'Use one of the login methods below to continue',
+    'or' => 'or',
+    'register_now' => 'register now',
+    'please_login' => 'Please login',
+    'email_short' => 'Email',
+    'password' => 'Password',
+    'remember' => 'Remember me',
+    'signin' => 'Sign in',
+    'forgot_password' => 'Forgot your password?',
+    'register' => 'Register',
+    'register_details' => 'Register details',
+    'register_confirmtext' => 'By clicking on confirm you are agreeing to the terms and conditions as set by',
+    'firstname' => 'Firstname',
+    'surname' => 'Surname',
+    'username' => 'Username',
+    'steamname' => 'Steam name',
+    'email' => 'Email adress',
+    'phonenumber' => 'Phone number',
+    'confirm_password' => 'Confirm password',
+    'submit' => 'Submit',
+    'verify_email' => 'Verify email',
+    'verifyed_email' => 'Email verified sucessfully',
+    'fresh_verification' => 'A fresh verification link has been sent to your email address.',
+    'check_mail' => 'Before proceeding, please check your email for a verification link.',
+    'mail_not_received' => 'If you did not receive the email,',
+    'request_verification' => 'click here to request another',
+    'reset_password' => 'Reset password',
+    'send_password_link' => 'Send password reset link',
+];
