57f0a3cab502974d838db5fc5acd3541951aa556
Kevin Als <kevin@tideo.dk>
2022-10-28 14:52:13 +0000

    Translated using Weblate (Danish)
    Currently translated at 100.0% (23 of 23 strings)
    
    Translation: Eventula Manager/Eventula Manager news component
    Translate-URL: https://translate.lan2play.de/projects/eventula-manager/eventula-manager-news-component/da/

--- src/lang/da/news.php
+++ src/lang/da/news.php
@@ -1,1 +1,33 @@
 <?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Default Text/Messages Lines
+    |--------------------------------------------------------------------------
+    |
+    |
+    */
+    'news' => 'Nyheder',
+    'comments' => 'Kommentarer',
+    'comment_reported' => 'Du har rapporteret denne kommentar!',
+    'post_comment' => 'Skriv en kommentar',
+    'login_to_post_comment' => 'Log venligst ind for at skrive en kommentar',
+    'news_tag' => 'Nyhedsmærke:',
+    'tagged_news' => 'Tagged nyheder:',
+    'please_login' => 'Vær venlig at logge ind',
+    'submit' => 'Indsend',
+    'comment_required' => 'En kommentar er påkrævet',
+    'comment_cannot_be_empty' => 'Kommentar må ikke være tom',
+    'comment_max_characters' => 'Kommentarer kan maksimalt være på 200 tegn',
+    'comment_cannot_post' => 'Kan ikke skrive en kommentar. Prøv igen.',
+    'comment_posted_waiting_approval' => 'Kommentar indsendt og venter på admin godkendelse!',
+    'comment_cannot_report' => 'Kan ikke rapportere kommentar. Prøv igen.',
+    'comment_reported_waiting_review' => 'Kommentar rapporteret og venter admin gennemgang!',
+    'comment_delete_not_your' => 'Dette er ikke din kommentar at slette!',
+    'comment_cannot_delete' => 'Kan ikke slette kommentar. Prøv igen.',
+    'comment_deleted' => 'Kommentar slettet!',
+    'comment_edit_not_your' => 'Dette er ikke din kommentar at redigere!',
+    'comment_cannot_edit' => 'Kan ikke redigere kommentar. Prøv igen.',
+    'comment_edited' => 'Kommentar redigeret!',
+    'unknownuser' => 'ukendt bruger',
+];
