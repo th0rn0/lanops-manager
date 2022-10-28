e529a00e1298d67d598c3fe607321de7b0a0d888
Kevin Als <kevin@tideo.dk>
2022-10-28 14:51:26 +0000

    Translated using Weblate (Danish)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: Eventula Manager/Eventula Manager passwords component
    Translate-URL: https://translate.lan2play.de/projects/eventula-manager/eventula-manager-passwords-component/da/

--- src/lang/da/passwords.php
+++ src/lang/da/passwords.php
@@ -1,1 +1,18 @@
 <?php
+return [
+    'reset' => 'Din adgangskode er blevet nulstillet!',
+    'sent' => 'Vi har sendt dit link til nulstilling af adgangskode via e-mail!',
+    'token' => 'Dette token til nulstilling af adgangskode er ugyldigt.',
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+    'password' => 'Adgangskoder skal være på mindst seks tegn og svare til bekræftelsen.',
+    'user' => "Vi kan ikke finde en bruger med den e-mailadresse.",
+];
