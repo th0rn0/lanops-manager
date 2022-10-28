a7e735b9772bd01e0427103faa028c8268fc0677
Kevin Als <kevin@tideo.dk>
2022-10-28 15:01:34 +0000

    Translated using Weblate (Danish)
    Currently translated at 10.7% (6 of 56 strings)
    
    Translation: Eventula Manager/Eventula Manager validation component
    Translate-URL: https://translate.lan2play.de/projects/eventula-manager/eventula-manager-validation-component/da/

--- src/lang/da/validation.php
+++ src/lang/da/validation.php
@@ -11,4 +11,9 @@ return [
     |
     */
     'accepted' => ':attribute skal accepteres.',
+    'active_url' => ':attribute er ikke en gyldig URL.',
+    'after' => ':attributte skal være en dato efter :date.',
+    'alpha' => ':attribute må kun indeholde bogstaver.',
+    'alpha_dash' => ':attribute  må kun indeholde bogstaver og nummere',
+    'alpha_num' => ':attribute må kun have bogstaver eller nummere',
 ];
