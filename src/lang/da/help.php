8a410e0fd934221b7766191fc6cd8594d7526bb7
Kevin Als <kevin@tideo.dk>
2022-10-28 14:56:48 +0000

    Translated using Weblate (Danish)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: Eventula Manager/Eventula Manager help component
    Translate-URL: https://translate.lan2play.de/projects/eventula-manager/eventula-manager-help-component/da/

--- src/lang/da/help.php
+++ src/lang/da/help.php
@@ -1,1 +1,20 @@
 <?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Default Text/Messages Lines
+    |--------------------------------------------------------------------------
+    |
+    |
+    */
+    'help' => 'Hjælp',
+    'event' => 'Event:',
+    'copylink' => 'Kopier Link:',
+    'search' => 'Søg..',
+    'showattachmentmanager' => 'Administrer vedhæftede filer',
+    'attachmentmanager' => 'Attachment Manager',
+    'uploadedattachments' => 'Uploadede vedhæftede filer',
+    'attachment' => 'Vedhæftet fil',
+    'attachments' => 'Vedhæftet filer',
+    'filename' => 'Filnavn',
+];
