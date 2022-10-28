91b92da17ffbf401610c4450676cc6288bb9cd0c
Kevin Als <kevin@tideo.dk>
2022-10-28 12:53:53 +0000

    Translated using Weblate (Danish)
    Currently translated at 100.0% (30 of 30 strings)
    
    Translation: Eventula Manager/Eventula Manager home component
    Translate-URL: https://translate.lan2play.de/projects/eventula-manager/eventula-manager-home-component/da/

--- src/lang/da/home.php
+++ src/lang/da/home.php
@@ -1,1 +1,43 @@
 <?php
+return [
+    'wantgetinaction' => 'Vil du være med',
+    'nothingtosee' => 'Intet at se her',
+    'eventsattended' => 'Begivenheder deltaget',
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'comingsoon' => 'Kommer snart',
+    'about' => 'Om',
+    'eventcalendar' => "Event kalender",
+    'seatsremaining' => "Pladser tilbage",
+    'ticketsremaining' => "Billetter tilbage",
+    'when' => "Hvornår",
+    'where' => "Hvor",
+    'price' => "Pris",
+    'ticketsstartfrom' => 'Billetter starter fra',
+    'latestnews' => 'Nyheder',
+    'thefam' => 'Mest aktive medlemmer hos :orgname',
+    'wins' => 'Wins',
+    'publicserver' => 'Public Server',
+    'allaboutorg' => 'Alt om :orgName',
+    'top5winners' => 'Top 5 vindere',
+    'servers_communication' => 'Kommunikation',
+    'servers_teamspeak' => 'Teamspeak Server',
+    'servers_mumble' => 'Muble Server',
+    'servers_facebook' => 'Facebook',
+    'servers_steam' => 'Steam',
+    'servers_reddit' => 'Reddit',
+    'servers_twitter' => 'Twitter',
+    'eventscomingsoon' => 'Begivenheder kommer snart',
+    'top5attendees' => 'Top 5 deltagere',
+    'servers_name' => 'Server navn',
+    'servers_join' => 'Join Server',
+    'servers_discord' => 'Discord',
+];
