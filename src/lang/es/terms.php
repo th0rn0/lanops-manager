3483a2eac294dd3a7e24c15cf9d5ad9777dab3b8
Bluesnow <danimoncada@gmail.com>
2022-11-15 18:38:45 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (3 of 3 strings)
    
    Translation: Eventula Manager/Eventula Manager terms component
    Translate-URL: https://translate.lan2play.de/projects/eventula-manager/eventula-manager-terms-component/es/

--- src/lang/es/terms.php
+++ src/lang/es/terms.php
@@ -1,1 +1,16 @@
 <?php
+return [
+    'registrationtermsandconditions' => 'Términos de registro y condiciones',
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'termsandconditions' => 'Términos y condiciones',
+    'purchaseandconditions' => 'Términos de compra y condiciones',
+];
