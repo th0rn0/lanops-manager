c4aaeea3abfb07f5af9f51d92419d8ad20f5aa90
Bluesnow <danimoncada@gmail.com>
2022-11-15 17:42:06 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: Eventula Manager/Eventula Manager help component
    Translate-URL: https://translate.lan2play.de/projects/eventula-manager/eventula-manager-help-component/es/

--- src/lang/es/help.php
+++ src/lang/es/help.php
@@ -1,1 +1,20 @@
 <?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Default Text/Messages Lines
+    |--------------------------------------------------------------------------
+    |
+    |
+    */
+    'help' => 'Ayuda',
+    'event' => 'Evento:',
+    'copylink' => 'Copiar Link:',
+    'search' => 'Buscar..',
+    'uploadedattachments' => 'Subir Archivos',
+    'showattachmentmanager' => 'Administrar archivos',
+    'attachmentmanager' => 'Gestor de archivos',
+    'attachment' => 'Archivo',
+    'attachments' => 'Archivos',
+    'filename' => 'Nombre de archivo',
+];
