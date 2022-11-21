fa5a020ffaaf92224ac9820fab7b7ecba9fc18be
Alexander Volz <Alexander@volzit.de>
2022-11-21 23:49:54 +0100

    reformat

--- src/app/ApiKey.php
+++ src/app/ApiKey.php
@@ -41,7 +41,7 @@ class ApiKey extends Model
      */
     public static function setPayPalUsername($username)
     {
- 		$key = self::where('key', 'paypal_username')->first();
+        $key = self::where('key', 'paypal_username')->first();
         $key->value = $username;
         if (!$key->save()) {
             return false;
@@ -54,54 +54,54 @@ class ApiKey extends Model
      * @param String $password
      * @return Boolean
      */
-	public static function setPayPalPassword($password)
+    public static function setPayPalPassword($password)
     {
- 		$key = self::where('key', 'paypal_password')->first();
+        $key = self::where('key', 'paypal_password')->first();
         $key->value = $password;
         if (!$key->save()) {
             return false;
         }
         return true;
     }
- 
- 	/**
+
+    /**
      * Set Paypal Signature
      * @param String $signature
      * @return Boolean
-     */	
- 	public static function setPayPalSignature($signature)
+     */
+    public static function setPayPalSignature($signature)
     {
- 		$key = self::where('key', 'paypal_signature')->first();
+        $key = self::where('key', 'paypal_signature')->first();
         $key->value = $signature;
         if (!$key->save()) {
             return false;
         }
         return true;
-    }    
+    }
 
-	/**
+    /**
      * Set Stripe Public Key
      * @param String $publicKey
      * @return Boolean
-     */	
- 	public static function setStripePublicKey($publicKey)
+     */
+    public static function setStripePublicKey($publicKey)
     {
- 		$key = self::where('key', 'stripe_public_key')->first();
+        $key = self::where('key', 'stripe_public_key')->first();
         $key->value = $publicKey;
         if (!$key->save()) {
             return false;
         }
         return true;
-    }    
+    }
 
-	/**
+    /**
      * Set Stripe Secret Key
      * @param String $secretKey
      * @return Boolean
-     */	
- 	public static function setStripeSecretKey($secretKey)
+     */
+    public static function setStripeSecretKey($secretKey)
     {
- 		$key = self::where('key', 'stripe_secret_key')->first();
+        $key = self::where('key', 'stripe_secret_key')->first();
         $key->value = $secretKey;
         if (!$key->save()) {
             return false;
@@ -113,7 +113,7 @@ class ApiKey extends Model
      * Set Challonge Api Key
      * @param String $apiKey
      * @return Boolean
-     */ 
+     */
     public static function setChallongeApiKey($apiKey)
     {
         $key = self::where('key', 'challonge_api_key')->first();
@@ -128,7 +128,7 @@ class ApiKey extends Model
      * Set Steam Api Key
      * @param String $apiKey
      * @return Boolean
-     */ 
+     */
     public static function setSteamApiKey($apiKey)
     {
         $key = self::where('key', 'steam_api_key')->first();
@@ -143,7 +143,7 @@ class ApiKey extends Model
      * Set Facebook App ID
      * @param String $appId
      * @return Boolean
-     */ 
+     */
     public static function setFacebookAppId($appId)
     {
         $key = self::where('key', 'facebook_app_id')->first();
@@ -158,7 +158,7 @@ class ApiKey extends Model
      * Set Facebook App Secret
      * @param String $appId
      * @return Boolean
-     */ 
+     */
     public static function setFacebookAppSecret($appSecret)
     {
         $key = self::where('key', 'facebook_app_secret')->first();
@@ -173,7 +173,7 @@ class ApiKey extends Model
      * Set Facebook Pixel ID
      * @param String $pixelId
      * @return Boolean
-     */ 
+     */
     public static function setFacebookPixelId($pixelId)
     {
         $key = self::where('key', 'facebook_pixel_id')->first();
@@ -188,7 +188,7 @@ class ApiKey extends Model
      * Set Facebook Pixel ID
      * @param String $pixelId
      * @return Boolean
-     */ 
+     */
     public static function setGoogleAnalyticsId($analyticsId)
     {
         $key = self::where('key', 'google_analytics_tracking_id')->first();
