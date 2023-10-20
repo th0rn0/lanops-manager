e1cf9b40aaf15d6a29615ab5843c3236489e889c
TheR00st3r <andyhahn91@gmail.com>
2023-10-21 00:58:24 +0200

    Fix Eventdashboard

--- src/resources/views/layouts/_partials/_admin/_event/dashMini.blade.php
+++ src/resources/views/layouts/_partials/_admin/_event/dashMini.blade.php
@@ -1,10 +1,10 @@
-<div class="card-deck">
+<div class="row">
 
 	@if ($event->status == 'PUBLISHED')
 		@php
 			$eventStatusColor = 'green';
 			$eventStatusIcon = 'fa-check-circle-o';
-		@endphp	
+		@endphp
 	@elseif ($event->status == 'REGISTEREDONLY')
 		@php
 			$eventStatusColor = 'blue';
@@ -26,114 +26,126 @@
 			$eventStatusIcon = 'fa-question-circle ';
 		@endphp
 	@endif
-	<div class="card event-card panel-{{ $eventStatusColor }} mb-3 d-flex">
-		<div class="card-header d-flex flex-wrap flex-grow-1">
-			<div class="icon-large pe-0">
-				<i class="fa {{ $eventStatusIcon }} fa-5x"></i>
-			</div>
-			<div class="ms-auto text-end">
-				<div class="huge ms-auto">
-					@if ($event->status == 'PUBLISHED')
-						<small>Live</small>
-					@elseif ($event->status == 'REGISTEREDONLY')
-						<small>Registered only</small>					
-					@elseif ($event->status == 'DRAFT')
-						<small>Draft</small>
-					@elseif ($event->status == 'PREVIEW')
-						<small>Preview</small>
-					@elseif ($event->status == 'PRIVATE')
-						<small>Private</small>
-					@else
-						<small>{{ $event->status }}</small>
-					@endif
+	<div class="col">
+		<div class="card event-card panel-{{ $eventStatusColor }} mb-3 d-flex">
+			<div class="card-header d-flex flex-wrap flex-grow-1">
+				<div class="icon-large pe-0">
+					<i class="fa {{ $eventStatusIcon }} fa-5x"></i>
 				</div>
-				<div>Event Status</div>
+				<div class="ms-auto text-end">
+					<div class="huge ms-auto">
+						@if ($event->status == 'PUBLISHED')
+							<small>Live</small>
+						@elseif ($event->status == 'REGISTEREDONLY')
+							<small>Registered only</small>
+						@elseif ($event->status == 'DRAFT')
+							<small>Draft</small>
+						@elseif ($event->status == 'PREVIEW')
+							<small>Preview</small>
+						@elseif ($event->status == 'PRIVATE')
+							<small>Private</small>
+						@else
+							<small>{{ $event->status }}</small>
+						@endif
+					</div>
+					<div>Event Status</div>
+				</div>
 			</div>
+			<a href="/admin/events/{{ $event->slug }}" class="card-footer d-flex flex-nowrap">
+				<span class="float-lext text-nowrap">View Event Page</span>
+				<span class="float-end ms-auto"><i class="fa fa-arrow-circle-right"></i></span>
+				<div class="clearfix"></div>
+			</a>
 		</div>
-		<a href="/admin/events/{{ $event->slug }}" class="card-footer d-flex flex-nowrap">
-			<span class="float-lext text-nowrap">View Event Page</span>
-			<span class="float-end ms-auto"><i class="fa fa-arrow-circle-right"></i></span>
-			<div class="clearfix"></div>
-		</a>
 	</div>
-	<div class="card event-card mb-3">
-		<div class="card-header d-flex flex-wrap flex-grow-1">
-			<div class="icon-large pe-0">
-				<i class="fa fa-wheelchair fa-5x"></i>
+	<div class="col">
+		<div class="card event-card mb-3">
+			<div class="card-header d-flex flex-wrap flex-grow-1">
+				<div class="icon-large pe-0">
+					<i class="fa fa-wheelchair fa-5x"></i>
+				</div>
+				<div class="col-auto ml-auto ms-auto text-end ps-0">
+					<div class="huge ms-auto">{{ $event->getSeatedCount() }}</div>
+					<div>Seated Participants</div>
+				</div>
 			</div>
-			<div class="col-auto ml-auto ms-auto text-end ps-0">
-				<div class="huge ms-auto">{{ $event->getSeatedCount() }}</div>
-				<div>Seated Participants</div>
-			</div>
+			<a href="/admin/events/{{ $event->slug }}/seating" class="card-footer d-flex flex-nowrap">
+				<span class="float-lext text-nowrap text-nowrap">View Seating Plans</span>
+				<span class="float-end ms-auto"><i class="fa fa-arrow-circle-right"></i></span>
+				<div class="clearfix"></div>
+			</a>
 		</div>
-		<a href="/admin/events/{{ $event->slug }}/seating" class="card-footer d-flex flex-nowrap">
-			<span class="float-lext text-nowrap text-nowrap">View Seating Plans</span>
-			<span class="float-end ms-auto"><i class="fa fa-arrow-circle-right"></i></span>
-			<div class="clearfix"></div>
-		</a>
 	</div>
-	<div class="card event-card mb-3">
-		<div class="card-header d-flex flex-wrap flex-grow-1">
-			<div class="icon-large pe-0">
-				<i class="fa fa-list-ol fa-5x"></i>
+	<div class="col">
+		<div class="card event-card mb-3">
+			<div class="card-header d-flex flex-wrap flex-grow-1">
+				<div class="icon-large pe-0">
+					<i class="fa fa-list-ol fa-5x"></i>
+				</div>
+				<div class="col-auto ml-auto ms-auto text-end ps-0">
+					<div class="huge ms-auto">{{ $event->tournaments->count() }}</div>
+					<div>Tournaments</div>
+				</div>
 			</div>
-			<div class="col-auto ml-auto ms-auto text-end ps-0">
-				<div class="huge ms-auto">{{ $event->tournaments->count() }}</div>
-				<div>Tournaments</div>
-			</div>
+			<a href="/admin/events/{{ $event->slug }}/tournaments" class="card-footer d-flex flex-nowrap">
+				<span class="float-lext text-nowrap">View Tournaments</span>
+				<span class="float-end ms-auto"><i class="fa fa-arrow-circle-right"></i></span>
+				<div class="clearfix"></div>
+			</a>
 		</div>
-		<a href="/admin/events/{{ $event->slug }}/tournaments" class="card-footer d-flex flex-nowrap">
-			<span class="float-lext text-nowrap">View Tournaments</span>
-			<span class="float-end ms-auto"><i class="fa fa-arrow-circle-right"></i></span>
-			<div class="clearfix"></div>
-		</a>
 	</div>
-	<div class="card event-card mb-3">
-		<div class="card-header d-flex flex-wrap flex-grow-1">
-			<div class="icon-large pe-0">
-				<i class="fa fa-user fa-5x"></i>
+	<div class="col">
+		<div class="card event-card mb-3">
+			<div class="card-header d-flex flex-wrap flex-grow-1">
+				<div class="icon-large pe-0">
+					<i class="fa fa-user fa-5x"></i>
+				</div>
+				<div class="col-auto ml-auto ms-auto text-end ps-0">
+					<div class="huge ms-auto">{{ $event->eventParticipants->count() }}</div>
+					<div>Attendees</div>
+				</div>
 			</div>
-			<div class="col-auto ml-auto ms-auto text-end ps-0">
-				<div class="huge ms-auto">{{ $event->eventParticipants->count() }}</div>
-				<div>Attendees</div>
-			</div>
+			<a href="/admin/events/{{ $event->slug }}/participants" class="card-footer d-flex flex-nowrap">
+				<span class="float-lext text-nowrap">View Attendees</span>
+				<span class="float-end ms-auto"><i class="fa fa-arrow-circle-right"></i></span>
+				<div class="clearfix"></div>
+			</a>
 		</div>
-		<a href="/admin/events/{{ $event->slug }}/participants" class="card-footer d-flex flex-nowrap">
-			<span class="float-lext text-nowrap">View Attendees</span>
-			<span class="float-end ms-auto"><i class="fa fa-arrow-circle-right"></i></span>
-			<div class="clearfix"></div>
-		</a>
 	</div>
-	<div class="card event-card mb-3">
-		<div class="card-header d-flex flex-wrap flex-grow-1">
-			<div class="icon-large pe-0">
-				<i class="fa fa-ticket fa-5x"></i>
+	<div class="col">
+		<div class="card event-card mb-3">
+			<div class="card-header d-flex flex-wrap flex-grow-1">
+				<div class="icon-large pe-0">
+					<i class="fa fa-ticket fa-5x"></i>
+				</div>
+				<div class="col-auto ml-auto ms-auto text-end ps-0">
+					<div class="huge ms-auto">{{ Settings::getCurrencySymbol() }}{{ $event->getTicketSalesCount() }}</div>
+					<div>Ticket Sales</div>
+				</div>
 			</div>
-			<div class="col-auto ml-auto ms-auto text-end ps-0">
-				<div class="huge ms-auto">{{ Settings::getCurrencySymbol() }}{{ $event->getTicketSalesCount() }}</div>
-				<div>Ticket Sales</div>
-			</div>
+			<a href="/admin/events/{{ $event->slug }}/tickets" class="card-footer d-flex flex-nowrap">
+				<span class="float-lext text-nowrap">View Tickets</span>
+				<span class="float-end ms-auto"><i class="fa fa-arrow-circle-right"></i></span>
+				<div class="clearfix"></div>
+			</a>
 		</div>
-		<a href="/admin/events/{{ $event->slug }}/tickets" class="card-footer d-flex flex-nowrap">
-			<span class="float-lext text-nowrap">View Tickets</span>
-			<span class="float-end ms-auto"><i class="fa fa-arrow-circle-right"></i></span>
-			<div class="clearfix"></div>
-		</a>
 	</div>
-	<div class="card event-card mb-3">
-		<div class="card-header d-flex flex-wrap flex-grow-1">
-			<div class="icon-large pe-0">
-				<i class="fa fa-calendar fa-5x"></i>
+	<div class="col">
+		<div class="card event-card mb-3">
+			<div class="card-header d-flex flex-wrap flex-grow-1">
+				<div class="icon-large pe-0">
+					<i class="fa fa-calendar fa-5x"></i>
+				</div>
+				<div class="col-auto ml-auto ms-auto text-end ps-0">
+					<div class="huge ms-auto">{{ $event->getTimetableDataCount() }}</div>
+					<div>Scheduled Slots</div>
+				</div>
 			</div>
-			<div class="col-auto ml-auto ms-auto text-end ps-0">
-				<div class="huge ms-auto">{{ $event->getTimetableDataCount() }}</div>
-				<div>Scheduled Slots</div>
-			</div>
+			<a href="/admin/events/{{ $event->slug }}/timetables" class="card-footer d-flex flex-nowrap">
+				<span class="float-lext text-nowrap">View Timetables</span>
+				<span class="float-end ms-auto"><i class="fa fa-arrow-circle-right"></i></span>
+				<div class="clearfix"></div>
+			</a>
 		</div>
-		<a href="/admin/events/{{ $event->slug }}/timetables" class="card-footer d-flex flex-nowrap">
-			<span class="float-lext text-nowrap">View Timetables</span>
-			<span class="float-end ms-auto"><i class="fa fa-arrow-circle-right"></i></span>
-			<div class="clearfix"></div>
-		</a>
 	</div>
 </div>
