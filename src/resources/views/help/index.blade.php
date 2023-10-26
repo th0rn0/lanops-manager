4c2cc8591a58427bd72b490ee53591e8ad670287
Bronko <code.keule@proton.me>
2023-10-26 21:25:34 +0200

    #571 fixed help entry second looks different

--- src/resources/views/help/index.blade.php
+++ src/resources/views/help/index.blade.php
@@ -14,7 +14,7 @@
 			<input id="searchtext" type="text" class="form-control" placeholder="@lang('help.search')" aria-label="searchtext" aria-describedby="search-addon">
 		</div>
 
-		<div class="card-group flex-column" id="helpentries" role="tablist" aria-multiselectable="false">
+		<!-- <div class="card-group flex-column" id="helpentries" role="tablist" aria-multiselectable="false">
 			@foreach ($helpCategorys as $helpCategory)
 
 				@foreach ($helpCategory->entrys as $entry)
@@ -82,7 +82,78 @@
 					});
 				</script>
 			@endforeach
+		</div> -->
+
+		<div class="accordion" id="helpentries" role="tablist" aria-multiselectable="false">
+			@foreach ($helpCategorys as $helpCategory)
+
+				@foreach ($helpCategory->entrys as $entry)
+					<div class="accordion-item @if(Colors::isBodyDarkMode()) border-light @endif">
+
+						<div class="accordion-header" role="tab" id="{{ $entry->nice_name }}">
+							<a role="button" data-bs-toggle="collapse" class="accordion-button accordion-arrow-toggle @if(!$loop->first) collapsed @endif" data-parent="#helpentries" href="#collapse_{{ $entry->nice_name }}" aria-expanded="false" aria-controls="collapse_{{ $entry->nice_name }}">
+								<h4 class="card-title m-0 d-inline flex-grow-1">
+									{{ $entry->display_name }}
+								</h4>
+								<div class="d-flex align-items-center">
+									<span class="badge text-bg-info">{{ $helpCategory->name }}</span>
+									@if ($entry->hasAttachment())
+										<span class="badge text-bg-info">@lang('help.attachment')</span>
+									@endif
+								</div>
+							</a>
+						</div>
+
+						<div id="collapse_{{ $entry->nice_name }}" class="accordion-collapse @if(!$loop->first) collapse @endif" role="tabpanel" aria-labelledby="{{ $entry->nice_name }}" data-parent="#helpentries">
+							<div class="accordion-body">
+								{!! $entry->content !!}
+								<div>
+									<table width="100%" class="table" id="dataTables-example">
+										<thead>
+											<tr>
+												<th>@lang('help.filename')</th>
+											</tr>
+										</thead>
+										<tbody>
+											@foreach ($entry->attachments as $attachment)
+												<tr>
+													<td>
+														<a href="{{ $attachment->path }}">{{ $attachment->display_name }}</a>
+													</td>
+												</tr>
+											@endforeach
+										</tbody>
+									</table>
+								</div>
+							</div>
+						</div>
+					</div>
+				@endforeach
+
+				<script>
+					function copyTextToClipBoard(copyText) {
+						const el = document.createElement('textarea');
+						el.value = copyText;
+						el.setAttribute('readonly', '');
+						el.style.position = 'absolute';
+						el.style.left = '-9999px';
+						document.body.appendChild(el);
+						el.select();
+						document.execCommand('copy');
+						document.body.removeChild(el);
+					}
+					document.addEventListener("DOMContentLoaded", function(event) {
+						$("#searchtext").on("keyup", function() {
+							var value = $(this).val().toLowerCase();
+							$("#helpentries .card").filter(function() {
+								$(this).toggle($(this).html().toLowerCase().indexOf(value) > -1)
+							});
+						});
+					});
+				</script>
+			@endforeach
 		</div>
+
 	</div>
 
 @endsection
