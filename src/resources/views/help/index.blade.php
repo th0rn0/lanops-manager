fdfd6fd86e3de5dd8c805d0f4105ba37ee6b23c0
Alexander Volz <Alexander@volzit.de>
2023-10-29 04:51:03 +0100

    fix search and one collapsed per help category,  readded copy button

--- src/resources/views/help/index.blade.php
+++ src/resources/views/help/index.blade.php
@@ -2,6 +2,31 @@
 
 @section ('page_title', Settings::getOrgName() . ' - ' . __('help.help'))
 
+
+@section ('scripts')
+<script>
+	function copyTextToClipBoard(copyText) {
+		const el = document.createElement('textarea');
+		el.value = copyText;
+		el.setAttribute('readonly', '');
+		el.style.position = 'absolute';
+		el.style.left = '-9999px';
+		document.body.appendChild(el);
+		el.select();
+		document.execCommand('copy');
+		document.body.removeChild(el);
+	}
+	document.addEventListener("DOMContentLoaded", function(event) {
+		$("#searchtext").on("keyup", function() {
+			var value = $(this).val().toLowerCase();
+			$("#helpentries .accordion-item").filter(function() {
+				$(this).toggle($(this).html().toLowerCase().indexOf(value) > -1)
+			});
+		});
+	});
+</script>
+@endsection
+
 @section ('content')
 
 <div class="container">
@@ -14,14 +39,17 @@
 			<input id="searchtext" type="text" class="form-control" placeholder="@lang('help.search')" aria-label="searchtext" aria-describedby="search-addon">
 		</div>
 
-		<div class="accordion" id="helpentries" role="tablist" aria-multiselectable="false">
+		<div class="accordion" id="helpentries" >
 			@foreach ($helpCategorys as $helpCategory)
 
 				@foreach ($helpCategory->entrys as $entry)
 					<div class="accordion-item @if(Colors::isBodyDarkMode()) border-light @endif">
 
 						<div class="accordion-header" role="tab" id="{{ $entry->nice_name }}">
-							<a role="button" data-bs-toggle="collapse" class="accordion-button accordion-arrow-toggle @if(!$loop->first) collapsed @endif" data-parent="#helpentries" href="#collapse_{{ $entry->nice_name }}" aria-expanded="false" aria-controls="collapse_{{ $entry->nice_name }}">
+							<div class="d-grid gap-2 d-md-flex justify-content-md-end">
+							<a class="btn btn-link justify-content-md-end" type="button" onclick="copyTextToClipBoard('{{url()->full()}}#{{ $entry->nice_name }}')" title="@lang('help.copylink')"><i class="far fa-clipboard"></i></a>
+							</div>
+							<a role="button" data-bs-toggle="collapse" class="accordion-button accordion-arrow-toggle @if( ($loop->parent->first && !$loop->first) || !$loop->parent->first) ) collapsed @endif" data-parent="#helpentries" href="#collapse_{{ $entry->nice_name }}" aria-expanded="false" aria-controls="collapse_{{ $entry->nice_name }}">
 								<h4 class="card-title m-0 d-inline flex-grow-1">
 									{{ $entry->display_name }}
 								</h4>
@@ -34,7 +62,7 @@
 							</a>
 						</div>
 
-						<div id="collapse_{{ $entry->nice_name }}" class="accordion-collapse @if(!$loop->first) collapse @endif" role="tabpanel" aria-labelledby="{{ $entry->nice_name }}" data-parent="#helpentries">
+						<div id="collapse_{{ $entry->nice_name }}" class="accordion-collapse @if(($loop->parent->first && !$loop->first) || !$loop->parent->first) ) collapse @endif" role="tabpanel" aria-labelledby="{{ $entry->nice_name }}" data-parent="#helpentries">
 							<div class="accordion-body">
 								{!! $entry->content !!}
 								<div>
@@ -60,27 +88,7 @@
 					</div>
 				@endforeach
 
-				<script>
-					function copyTextToClipBoard(copyText) {
-						const el = document.createElement('textarea');
-						el.value = copyText;
-						el.setAttribute('readonly', '');
-						el.style.position = 'absolute';
-						el.style.left = '-9999px';
-						document.body.appendChild(el);
-						el.select();
-						document.execCommand('copy');
-						document.body.removeChild(el);
-					}
-					document.addEventListener("DOMContentLoaded", function(event) {
-						$("#searchtext").on("keyup", function() {
-							var value = $(this).val().toLowerCase();
-							$("#helpentries .card").filter(function() {
-								$(this).toggle($(this).html().toLowerCase().indexOf(value) > -1)
-							});
-						});
-					});
-				</script>
+
 			@endforeach
 		</div>
 
