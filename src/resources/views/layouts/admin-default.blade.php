dfe73e6931b067e0ab86be65f44fc612b6001426
Alexander Volz <Alexander@volzit.de>
2023-11-06 17:55:28 +0100

    centralise seo stuff and add Lan2play Eventula Manager to desc and keywords

--- src/resources/views/layouts/admin-default.blade.php
+++ src/resources/views/layouts/admin-default.blade.php
@@ -1,96 +1,109 @@
-<!DOCTYPE html> <html lang="en"data-bs-theme="{{Colors::getTheme()}}"> <head> <meta charset="utf-8"> <meta http-equiv="X-UA-Compatible"
-	content="IE=edge"> <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="description" content=""> <meta name="author" content=""> <title> @hasSection ('page_title')
-	@yield('page_title') | {{ Settings::getOrgName() }} @else {{ Settings::getOrgName() }} Admin @endif </title>
-<link rel="icon" type="image/png" sizes="32x32" href="{{ Settings::getOrgFavicon() }}">
+<!DOCTYPE html>
+<html lang="en"data-bs-theme="{{ Colors::getTheme() }}">
 
-<!-- Admin CSS -->
-		<link href="/css/admin.css" rel="stylesheet">
-		<link href="/css/jquery-ui.min.css" rel="stylesheet">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    {!! SEOMeta::generate() !!}
+    <title>
+        @hasSection ('page_title')
+            @yield('page_title') | {{ Settings::getOrgName() }}
+        @else
+            {{ Settings::getOrgName() }} Admin
+        @endif
+    </title>
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ Settings::getOrgFavicon() }}">
 
-		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-		<!--[if lt IE 9]>
-				<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-				<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-		<![endif]-->
+    <!-- Admin CSS -->
+    <link href="/css/admin.css" rel="stylesheet">
+    <link href="/css/jquery-ui.min.css" rel="stylesheet">
 
-<!-- Jquery Core JavaScript -->
-		<script src="/js/vendor.js"></script>
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+  <![endif]-->
 
-		<!-- Morris Charts JavaScript -->
-		<!-- <script src="/js/morris/raphael.min.js"></script>
-		<script src="/js/morris/morris.min.js"></script>
-		<script src="/js/morris/morris-data.js"></script> -->
+    <!-- Jquery Core JavaScript -->
+    <script src="/js/vendor.js"></script>
 
-		<script>
-			function ConfirmDelete()
-			{
-				var x = confirm("Are you sure you want to delete?");
-				if (x)
-					return true;
-				else
-					return false;
-			}
-			function ConfirmSubmit()
-			{
-				var x = confirm("Are you sure you want to submit?");
-				if (x)
-					return true;
-				else
-					return false;
-			}
-	        jQuery(document).ready(function() {
-	            jQuery('.wysiwyg-editor').summernote({
-	              height:300,
-	            });
-	            jQuery('.wysiwyg-editor-small').summernote({
-	              height:150,
-	            });
-	        });
-		</script>
+    <!-- Morris Charts JavaScript -->
+    <!-- <script src="/js/morris/raphael.min.js"></script>
+  <script src="/js/morris/morris.min.js"></script>
+  <script src="/js/morris/morris-data.js"></script> -->
 
-	</head>
-	<body>
-		<!-- <div id="wrapper"> -->
-@include ('layouts._partials._admin.topnavigation')
-<div class="container-fluid">
-	<div class="row">
+    <script>
+        function ConfirmDelete() {
+            var x = confirm("Are you sure you want to delete?");
+            if (x)
+                return true;
+            else
+                return false;
+        }
 
-		@include ('layouts._partials._admin.sidenavigation')
-		<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+        function ConfirmSubmit() {
+            var x = confirm("Are you sure you want to submit?");
+            if (x)
+                return true;
+            else
+                return false;
+        }
+        jQuery(document).ready(function() {
+            jQuery('.wysiwyg-editor').summernote({
+                height: 300,
+            });
+            jQuery('.wysiwyg-editor-small').summernote({
+                height: 150,
+            });
+        });
+    </script>
+
+</head>
+
+<body>
+    <!-- <div id="wrapper"> -->
+    @include ('layouts._partials._admin.topnavigation')
+    <div class="container-fluid">
+        <div class="row">
+
+            @include ('layouts._partials._admin.sidenavigation')
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
 
 
-			@yield('content')
+                @yield('content')
 
-			<div class="alert-fixed">
-				@foreach (['danger', 'warning', 'success', 'info'] as $msg)
-				@if (Session::has('alert-' . $msg))
-				<p class="alert  alert-{{ $msg }} alert-dismissible fade show">
-					<b>{{ Session::get('alert-' . $msg) }}</b> <a href="#" class="btn-close text-decoration-none" data-bs-dismiss="alert"
-						aria-label="close"></a>
-				</p>
-				@endif
-				@endforeach
-			</div>
-			@if ($errors->any())
-			<div class="alert alert-fixed alert-danger alert-dismissible fade show" role="alert">
-				<h4 mt-0>Errors occured</h4>
-				<ul>
-					@foreach ($errors->all() as $error)
-					<li>{{ $error }}</li>
-					@endforeach
+                <div class="alert-fixed">
+                    @foreach (['danger', 'warning', 'success', 'info'] as $msg)
+                        @if (Session::has('alert-' . $msg))
+                            <p class="alert  alert-{{ $msg }} alert-dismissible fade show">
+                                <b>{{ Session::get('alert-' . $msg) }}</b> <a href="#"
+                                    class="btn-close text-decoration-none" data-bs-dismiss="alert"
+                                    aria-label="close"></a>
+                            </p>
+                        @endif
+                    @endforeach
+                </div>
+                @if ($errors->any())
+                    <div class="alert alert-fixed alert-danger alert-dismissible fade show" role="alert">
+                        <h4 mt-0>Errors occured</h4>
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
 
-				</ul>
-				<button type="button" class="btn-close text-decoration-none" data-bs-dismiss="alert" aria-label="Close">
+                        </ul>
+                        <button type="button" class="btn-close text-decoration-none" data-bs-dismiss="alert"
+                            aria-label="Close">
 
-				</button>
-			</div>
-			@endif
-		</main>
-	</div>
-</div>
-<!-- </div> -->
+                        </button>
+                    </div>
+                @endif
+            </main>
+        </div>
+    </div>
+    <!-- </div> -->
 </body>
 
 </html>
