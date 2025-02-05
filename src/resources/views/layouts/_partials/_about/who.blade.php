fcde42251862dca59ce10c804fb6f665430fd717
Thornton Phillis <dev@th0rn0.co.uk>
2025-02-05 18:21:50 +0000

    more info

--- src/resources/views/layouts/_partials/_about/who.blade.php
+++ src/resources/views/layouts/_partials/_about/who.blade.php
@@ -1,20 +1,18 @@
 <div>
-    <h4>BYOC Crew</h4>
     <table class="table table-bordered">
         <tbody>
             <tr>
                 <td>
-                    <h4>Event Team:</h4>
+                    <h4>Management Team:</h4>
                     <ul>
                         <li>Th0rn0</li>
                         <li>Shlee</li>
-                        <li>Alan</li>
+                        <li>Alan Johnson</li>
                     </ul>
                 </td>
                 <td>
                     <h4>Technical Team:</h4>
                     <ul>
-                        <li>Rebel</li>
                         <li>Raz3r</li>
                         <li>Gnome</li>
                     </ul>
@@ -24,31 +22,6 @@
                     <ul>
                         <li>Val</li>
                         <li>Pete</li>
-                    </ul>
-                </td>
-            </tr>
-        </tbody>
-    </table>
-    <h4>Community Crew</h4>
-    <table class="table table-bordered">
-        <tbody>
-            <tr>
-                <td>
-                    <h4>Club Team:</h4>
-                    <ul>
-                        <li>Th0rn0</li>
-                        <li>Rebel</li>
-                        <li>Shlee</li>
-                    </ul>
-                </td>
-                <td>
-                    <h4>Alumnus Team:</h4>
-                    <ul>
-                        <li>Storm Seeker</li>
-                        <li>Kayomani</li>
-                        <li>Invert</li>
-                        <li>Bruno</li>
-                        <li>Rosco</li>
                     </ul>
                 </td>
             </tr>
