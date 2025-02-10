711f736c7f64e466160cf31743793c36c0ac15ef
Thornton Phillis <dev@th0rn0.co.uk>
2025-02-10 21:38:29 +0000

    cleanup

--- src/app/Console/Kernel.php
+++ src/app/Console/Kernel.php
@@ -26,8 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('sitemap:generate')
-            // ->hourly()
-            ->everyMinute()
+            ->hourly()
             ->onOneServer()
             ->runInBackground();
         $schedule->command('bot:timetable-updates')
